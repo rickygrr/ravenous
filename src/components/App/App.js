3c705813080254c8ab4e28127a9f17e04481ce7a
Ricardo Gomez <rgomez@gatech.edu>
2018-07-30 14:13:53 -0600

    Tutorial pt. 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,26 +16,28 @@ const business = {
     reviewCount: 90
 }
 const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
+    business,
+    business,
+    business,
+    business,
+    business,
+    business
 ];
 
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList businesses={businesses} />
-      </div>
-    );
-  }
+    searchYelp(term, location, sortBy) {
+        console.log("Searching Yelp with " + term + ", " + location + ", " + sortBy);
+    }
+    render() {
+        return (
+            <div className="App">
+              <h1>ravenous</h1>
+              <SearchBar searchYelp={this.searchYelp} />
+              <BusinessList businesses={businesses} />
+            </div>
+        );
+    }
 }
 
 export default App;
-
